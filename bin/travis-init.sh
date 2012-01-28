35e34fd3739df6af7648b822ea59f223ff595e88
Benjamin Steininger <robo47@robo47.net>
2012-01-28 21:04:16 +0100

    Travis-CI
     - detects DB from ENV-variables, defaults to sqlite
     - automatically setups the tables and loads fixture in sfPluginTestBootstrap::setup

---
+++ bin/travis-init.sh
@@ -0,0 +1,4 @@
+echo "Cloning symfony"
+git clone git://github.com/symfony/symfony1.git lib/vendor/symfony
+
+echo "exporting symfony ENV-Variable"
--- test/bootstrap/sfPluginTestBootstrap.class.php
+++ test/bootstrap/sfPluginTestBootstrap.class.php
@@ -6,29 +6,51 @@ class sfPluginTestBootstrap
     $symfonyDir     = null,
     $configuration  = null,
     $context        = null;
+  /**
+   *
+   * @var sfDatabaseManager
+   */
+  protected
+    $databasemanager = null;
 
   public function __construct($symfonyDir = null)
   {
-    if ($symfonyDir) 
+    if ($symfonyDir)
     {
       $this->setSymfonyDir($symfonyDir);
     }
-    
+
     $this->setSymfonyPluginsDir(dirname(__FILE__).'/../../..');
   }
-  
+
   public function teardown()
   {
     sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/cache');
     sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/log');
   }
-  
+
   public function setup()
   {
-    // Copy over test SQLite database
-    copy(dirname(__FILE__).'/../fixtures/project/data/fresh_test_db.sqlite', dirname(__FILE__).'/../fixtures/project/data/test.sqlite');
+    $config = $this->configuration->getPluginConfiguration('sfDoctrinePlugin')->getCliConfig();
+
+    //$this->databasemanager->loadConfiguration();
+
+    $db = $this->databasemanager->getDatabase('doctrine');
+    /* @var $db sfDoctrineDatabase */
+
+    try {
+        // ignore error if database does not yet exist (clean CI-env)
+        $db->getDoctrineConnection()->dropDatabase();
+    } catch (Exception $e) {
+    }
+    $db->getDoctrineConnection()->createDatabase();
+
+
+    Doctrine_Core::loadModels($config['models_path'], Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+    Doctrine_Core::createTablesFromArray(Doctrine_Core::getLoadedModels());
+    Doctrine_Core::loadData(dirname(__FILE__).'/../fixtures/project/data/fixtures/categories.yml');
   }
-  
+
   // Find all tests and run them
   public function run()
   {
@@ -37,46 +59,46 @@ class sfPluginTestBootstrap
 
     exit($h->run() ? 0 : 1);
   }
-  
+
   public function autoload($class)
   {
     $autoload = sfSimpleAutoload::getInstance();
     $autoload->reload();
     return $autoload->autoload($class);
   }
-  
+
   public function getConfiguration()
   {
     return $this->configuration;
   }
-  
+
   public function getContext()
   {
     return $this->context;
   }
-  
+
   public function setSymfonyDir($symfonyDir)
   {
-    if ($symfonyDir) 
+    if ($symfonyDir)
     {
       $this->symfonyDir   = $symfonyDir;
       $_SERVER['SYMFONY'] = $symfonyDir;
       file_put_contents('/tmp/symfony_dir', $this->symfonyDir);
     }
   }
-  
+
   public function setSymfonyPluginsDir($pluginsDir)
   {
     $this->pluginsDir               = $pluginsDir;
     $_SERVER['SYMFONY_PLUGINS_DIR'] = $pluginsDir;
   }
-  
+
   public function getSymfonyDir()
   {
-    if (!$this->symfonyDir) 
+    if (!$this->symfonyDir)
     {
       // Get path to symfony
-      if (isset($_SERVER['SYMFONY'])) 
+      if (isset($_SERVER['SYMFONY']))
       {
         $this->symfonyDir = $_SERVER['SYMFONY'];
       }
@@ -91,10 +113,10 @@ class sfPluginTestBootstrap
         throw new Exception(sprintf("Symfony directory%s not found.  Please set \$_SERVER['SYMFONY'] or provide a --symfony_dir argument", isset($_SERVER['SYMFONY']) ? " '$_SERVER[SYMFONY]'" : ''));
       }
     }
-    
+
     return $this->symfonyDir;
   }
-  
+
   public function bootstrap($app = 'frontend', $debug = true)
   {
     // so that all notices will appear
@@ -106,17 +128,26 @@ class sfPluginTestBootstrap
 
     // Create configuration and context
     require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
+
+    $dbms = 'sqlite';
+    if (isset($_SERVER['DB'])) {
+        $dbms = strtolower($_SERVER['DB']);
+    }
+
+    copy(dirname(__FILE__).'/../fixtures/project/config/database-' . $dbms . '.yml',
+         dirname(__FILE__).'/../fixtures/project/config/databases.yml');
+
     $this->configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', $debug);
-    
+
     require_once $this->configuration->getSymfonyLibDir().'/vendor/lime/lime.php';
     $this->context = sfContext::createInstance($this->configuration);
-    
-    new sfDatabaseManager($this->configuration);
+
+    $this->databasemanager = new sfDatabaseManager($this->configuration);
 
     // Register teardown / autoload functions
     spl_autoload_register(array($this, 'autoload'));
     register_shutdown_function(array($this, 'teardown'));
-    
+
     // Cleanup and copy over SQLite DB
     $this->teardown();
     $this->setup();
